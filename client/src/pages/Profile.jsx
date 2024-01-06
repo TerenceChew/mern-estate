1699d4e2d8ca339038a7935539387cbb5821abec
TerenceChew <wschew97@gmail.com>
2024-01-06 23:12:30 +0800

    Remove side effect in Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -173,10 +173,6 @@ export default function Profile() {
     }
   }, [imageFile]);
 
-  useEffect(() => {
-    setShowUpdateSuccessMessage(false);
-  }, [error, deleteRequested]);
-
   return (
     <>
       <main
