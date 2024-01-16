de1694ed07c279631c2be810ba4643687c7f2dcb
TerenceChew <wschew97@gmail.com>
2024-01-16 16:33:59 +0800

    Update CreateListing page functionality

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const listingSchema = new Schema(
   {
-    name: {
+    title: {
       type: String,
       required: true,
     },
--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -7,20 +7,40 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase.js";
 import { generateUniqueFileName } from "../utils/utilities";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateListing() {
+  const { currentUser } = useSelector((state) => state.user);
   const [imageFiles, setImageFiles] = useState([]);
   const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    address: "",
+    type: "sale",
+    parking: false,
+    furnished: false,
+    offer: false,
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountPrice: 0,
     imageUrls: [],
   });
   const [isUploadingFiles, setIsUploadingFiles] = useState(false);
   const [fileUploadError, setFileUploadError] = useState(null);
+  const [submitError, setSubmitError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const imageFileInputRef = useRef();
+  const navigate = useNavigate();
 
   // For managing image file input
   const resetImageFileInput = () => {
     imageFileInputRef.current.value = null;
   };
+
+  // Validations
+  const isImageUrlsEmpty = () => !formData.imageUrls.length;
 
   // Handler functions
   const handleFileInputChange = (e) => {
@@ -78,6 +98,66 @@ export default function CreateListing() {
       imageUrls: updatedImageUrls,
     });
   };
+  const handleChange = (e) => {
+    const { value, checked, type, name } = e.target;
+
+    if (name === "sale" || name === "rent") {
+      setFormData({
+        ...formData,
+        type: name,
+      });
+    } else if (name === "offer" && !checked) {
+      setFormData({
+        ...formData,
+        [name]: checked,
+        discountPrice: 0,
+      });
+    } else if (type === "number") {
+      setFormData({
+        ...formData,
+        [name]: Number(value),
+      });
+    } else if (type === "checkbox") {
+      setFormData({
+        ...formData,
+        [name]: checked,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (isImageUrlsEmpty())
+      return setSubmitError("A listing must have at least one image");
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...formData, userRef: currentUser._id }),
+      });
+      const data = await res.json();
+
+      if (res.ok) {
+        setSubmitError(null);
+        navigate(`/listing/${data._id}`);
+      } else {
+        setSubmitError(data.message);
+      }
+    } catch (err) {
+      setSubmitError("Failed to handle submit for create listing");
+    }
+
+    setLoading(false);
+  };
 
   // For uploading image file
   const uploadImageFile = (imageFile) => {
@@ -116,17 +196,22 @@ export default function CreateListing() {
       <article className="w-64 xs:w-full xs:max-w-72 sm:max-w-md xl:max-w-5xl flex flex-col items-center gap-8">
         <h1 className="font-semibold text-2xl sm:text-3xl">Create a Listing</h1>
 
-        <form className="w-full flex flex-col xl:flex-row xl:flex-wrap gap-4 xl:gap-6">
+        <form
+          className="w-full flex flex-col xl:flex-row xl:flex-wrap gap-4 xl:gap-6"
+          onSubmit={handleSubmit}
+        >
           <div className="flex flex-col flex-1 gap-4">
             <input
               className="border border-gray-200 focus:outline-gray-300 rounded-lg p-2.5 sm:p-3"
               type="text"
-              id="name"
-              name="name"
-              aria-label="Name"
-              placeholder="Name"
+              id="title"
+              name="title"
+              aria-label="Title"
+              placeholder="Title"
               minLength="10"
               maxLength="60"
+              value={formData.title}
+              onChange={handleChange}
               required
             />
             <textarea
@@ -135,6 +220,8 @@ export default function CreateListing() {
               name="description"
               aria-label="Description"
               placeholder="Description"
+              value={formData.description}
+              onChange={handleChange}
               required
             />
             <input
@@ -146,6 +233,8 @@ export default function CreateListing() {
               placeholder="Address"
               minLength="10"
               maxLength="60"
+              value={formData.address}
+              onChange={handleChange}
               required
             />
 
@@ -153,12 +242,14 @@ export default function CreateListing() {
               <div className="flex items-center gap-2">
                 <input
                   className="w-4 h-4 cursor-pointer"
-                  id="sell"
-                  name="sell"
+                  id="sale"
+                  name="sale"
                   type="checkbox"
+                  checked={formData.type === "sale"}
+                  onChange={handleChange}
                 />
-                <label className="font-semibold" htmlFor="sell">
-                  Sell
+                <label className="font-semibold" htmlFor="sale">
+                  Sale
                 </label>
               </div>
 
@@ -168,6 +259,8 @@ export default function CreateListing() {
                   id="rent"
                   name="rent"
                   type="checkbox"
+                  checked={formData.type === "rent"}
+                  onChange={handleChange}
                 />
                 <label className="font-semibold" htmlFor="rent">
                   Rent
@@ -180,6 +273,8 @@ export default function CreateListing() {
                   id="parking"
                   name="parking"
                   type="checkbox"
+                  checked={formData.parking}
+                  onChange={handleChange}
                 />
                 <label className="font-semibold" htmlFor="parking">
                   Parking
@@ -192,6 +287,8 @@ export default function CreateListing() {
                   id="furnished"
                   name="furnished"
                   type="checkbox"
+                  checked={formData.furnished}
+                  onChange={handleChange}
                 />
                 <label className="font-semibold" htmlFor="furnished">
                   Furnished
@@ -204,6 +301,8 @@ export default function CreateListing() {
                   id="offer"
                   name="offer"
                   type="checkbox"
+                  checked={formData.offer}
+                  onChange={handleChange}
                 />
                 <label className="font-semibold" htmlFor="offer">
                   Offer
@@ -218,9 +317,10 @@ export default function CreateListing() {
                   id="bedrooms"
                   name="bedrooms"
                   type="number"
-                  defaultValue={1}
                   min="1"
-                  max="15"
+                  max="20"
+                  value={formData.bedrooms.toString()} // A trick to remove leading 0
+                  onChange={handleChange}
                   required
                 />
                 <label className="font-semibold" htmlFor="bedrooms">
@@ -234,9 +334,10 @@ export default function CreateListing() {
                   id="bathrooms"
                   name="bathrooms"
                   type="number"
-                  defaultValue={1}
                   min="1"
-                  max="15"
+                  max="20"
+                  value={formData.bathrooms.toString()} // A trick to remove leading 0
+                  onChange={handleChange}
                   required
                 />
                 <label className="font-semibold" htmlFor="bathrooms">
@@ -250,28 +351,38 @@ export default function CreateListing() {
                   id="regularPrice"
                   name="regularPrice"
                   type="number"
-                  defaultValue={0}
-                  min="1"
+                  min="50"
+                  max="100000000"
+                  value={formData.regularPrice.toString()} // A trick to remove leading 0
+                  onChange={handleChange}
                   required
                 />
                 <label className="font-semibold" htmlFor="regularPrice">
-                  Regular price ($/Month)
+                  {`Regular price ${
+                    formData.type === "rent" ? "($/Month)" : ""
+                  }`}
                 </label>
               </div>
 
-              <div className="flex items-center gap-2.5">
-                <input
-                  className="w-24 p-2 rounded-lg focus:outline-gray-300"
-                  id="discountPrice"
-                  name="discountPrice"
-                  type="number"
-                  defaultValue={0}
-                  min="1"
-                />
-                <label className="font-semibold" htmlFor="discountPrice">
-                  Discount price ($/Month)
-                </label>
-              </div>
+              {formData.offer && (
+                <div className="flex items-center gap-2.5">
+                  <input
+                    className="w-24 p-2 rounded-lg focus:outline-gray-300"
+                    id="discountPrice"
+                    name="discountPrice"
+                    type="number"
+                    min="0"
+                    max="100000000"
+                    value={formData.discountPrice.toString()} // A trick to remove leading 0
+                    onChange={handleChange}
+                  />
+                  <label className="font-semibold" htmlFor="discountPrice">
+                    {`Discount price ${
+                      formData.type === "rent" ? "($/Month)" : ""
+                    }`}
+                  </label>
+                </div>
+              )}
             </div>
           </div>
 
@@ -287,7 +398,6 @@ export default function CreateListing() {
                 type="file"
                 accept="image/*"
                 multiple
-                required
                 aria-label="Select images to upload"
                 onChange={handleFileInputChange}
                 ref={imageFileInputRef}
@@ -339,9 +449,21 @@ export default function CreateListing() {
               </div>
             )}
 
-            <button className="bg-slate-700 hover:bg-slate-800 text-white rounded-lg p-2.5 sm:p-3 disabled:opacity-80 disabled:pointer-events-none">
+            <button
+              className="bg-slate-700 hover:bg-slate-800 text-white rounded-lg p-2.5 sm:p-3 disabled:opacity-80 disabled:pointer-events-none"
+              disabled={loading}
+            >
               CREATE LISTING
             </button>
+
+            {submitError && (
+              <p
+                className="text-red-600 text-center"
+                aria-label="Error message"
+              >
+                {submitError}
+              </p>
+            )}
           </div>
         </form>
       </article>
