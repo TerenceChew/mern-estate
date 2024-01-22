9c9371c36b599c1e4bc80289dcea3aa7e3daae6b
TerenceChew <wschew97@gmail.com>
2024-01-22 23:17:23 +0800

    Create delete listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { generateError } from "../utils/error.js";
 
 export const handleCreateListing = async (req, res, next) => {
   try {
@@ -7,6 +8,31 @@ export const handleCreateListing = async (req, res, next) => {
     await newListing.save();
 
     res.status(201).json(newListing);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleDeleteListing = async (req, res, next) => {
+  const { decodedUser, params, body } = req;
+  const currentUserId = body.id;
+  const listingId = params.id;
+
+  if (decodedUser.id !== currentUserId)
+    return next(generateError(401, "You can only delete your own listing!"));
+
+  try {
+    const listingToDelete = await Listing.findOne({ _id: listingId });
+
+    if (!listingToDelete)
+      return next(
+        generateError(404, "Can't delete. Listing with given ID not found!")
+      );
+
+    await Listing.deleteOne({ _id: listingId });
+    const remainingListings = await Listing.find({ userRef: currentUserId });
+
+    res.status(200).json(remainingListings);
   } catch (err) {
     next(err);
   }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyJwtToken } from "../utils/verifyUser.js";
-import { handleCreateListing } from "../controllers/listing.controller.js";
+import {
+  handleCreateListing,
+  handleDeleteListing,
+} from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyJwtToken, handleCreateListing);
+router.delete("/delete/:id", verifyJwtToken, handleDeleteListing);
 
 export default router;
