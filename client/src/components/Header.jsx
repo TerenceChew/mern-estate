0ba74d659ecc01d2942425b89bc7defd867fbfda
TerenceChew <wschew97@gmail.com>
2024-02-13 16:14:32 +0800

    Update search functionality in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Handler functions
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set("searchTerm", searchTerm);
+    const newQueryString = searchParams.toString();
+
+    navigate(`/search?${newQueryString}`);
+  };
+
+  // Side effects
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const searchTermFromParams = searchParams.get("searchTerm");
+
+    if (searchTermFromParams) {
+      setSearchTerm(searchTermFromParams);
+    }
+  }, [location.search]);
 
   return (
     <header className="h-[140px] sm:h-20 flex flex-col sm:flex-row justify-between items-center px-3 sm:px-4 lg:px-8 py-3 bg-slate-200 shadow-md relative z-20">
@@ -14,13 +40,22 @@ export default function Header() {
         </h1>
       </Link>
 
-      <form className="w-64 sm:w-56 md:w-72 2xl:w-96 flex justify-between items-center bg-slate-50 rounded-lg px-3 py-1.5 sm:py-2.5">
+      <form
+        className="w-64 sm:w-56 md:w-72 2xl:w-96 flex justify-between items-center bg-slate-50 rounded-lg px-3 py-1.5 sm:py-2.5"
+        onSubmit={handleSubmit}
+      >
         <input
           className="bg-transparent w-10/12 focus:outline-none"
           type="text"
-          placeholder="Search..."
-        ></input>
-        <FaSearch className="hover:cursor-pointer" />
+          placeholder="Search listings"
+          aria-label="Search listings"
+          name="searchTerm"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <button>
+          <FaSearch className="hover:cursor-pointer text-lg text-slate-700" />
+        </button>
       </form>
 
       <nav className="w-48 sm:w-44 md:w-48 lg:w-52 2xl:w-56 flex justify-between items-center text-sm sm:text-base">
