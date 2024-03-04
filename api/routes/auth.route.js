1bedb378b65a5c484fa4605e090cab1cdb7c4b18
TerenceChew <wschew97@gmail.com>
2024-03-05 01:38:35 +0800

    Add input validation to sign up api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -5,10 +5,11 @@ import {
   handleGoogleSignIn,
   handleSignOut,
 } from "../controllers/auth.controller.js";
+import { validateSignUp } from "../validations/auth.validation.js";
 
 const router = express.Router();
 
-router.post("/sign-up", handleSignUp);
+router.post("/sign-up", validateSignUp, handleSignUp);
 router.post("/sign-in", handleSignIn);
 router.post("/google-sign-in", handleGoogleSignIn);
 router.post("/sign-out", handleSignOut);
