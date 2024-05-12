df489c206f69ada2b5ce62e2dd948f4c6f2eeeb0
TerenceChew <wschew97@gmail.com>
2024-05-13 00:06:04 +0800

    Feature: Add password confirmation to Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -33,6 +33,7 @@ export default function Profile() {
     username: currentUser.username,
     email: currentUser.email,
     password: "",
+    passwordConfirmation: "",
   });
   const [showUpdateSuccessMessage, setShowUpdateSuccessMessage] =
     useState(false);
@@ -43,31 +44,38 @@ export default function Profile() {
 
   // Validation
   const validate = (formData) => {
+    const { username, email, password, passwordConfirmation } = formData;
     const errors = {};
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/;
 
-    if (!formData.username) {
+    if (!username) {
       errors.username = "Please enter a valid username!";
-    } else if (formData.username.length < 5) {
+    } else if (username.length < 5) {
       errors.username = "Username must be at least 5 characters!";
-    } else if (formData.username.length > 20) {
+    } else if (username.length > 20) {
       errors.username = "Username cannot be more than 20 characters!";
     }
 
-    if (!formData.email || !emailRegex.test(formData.email)) {
+    if (!email || !emailRegex.test(email)) {
       errors.email = "Please enter a valid email!";
     }
 
-    if (!formData.password) {
+    if (!password) {
       errors.password = "Please enter your current or new password!";
-    } else if (formData.password.length < 8) {
+    } else if (password.length < 8) {
       errors.password = "Password must be at least 8 characters!";
-    } else if (formData.password.length > 16) {
+    } else if (password.length > 16) {
       errors.password = "Password cannot be more than 16 characters!";
-    } else if (!passwordRegex.test(formData.password)) {
+    } else if (!passwordRegex.test(password)) {
       errors.password =
         "Password must contain at least 1 digit, 1 uppercase and 1 lowercase letter!";
+    }
+
+    if (!passwordConfirmation) {
+      errors.passwordConfirmation = "Please re-enter your password here!";
+    } else if (passwordConfirmation !== password) {
+      errors.passwordConfirmation = "Does not match password!";
     }
 
     return errors;
@@ -353,6 +361,19 @@ export default function Profile() {
             />
             <p className="text-center text-red-600">
               {validationErrors.password || serverValidationErrors.password}
+            </p>
+            <input
+              className="border border-gray-200 focus:outline-gray-300 rounded-lg p-2.5 sm:p-3"
+              type="password"
+              id="passwordConfirmation"
+              name="passwordConfirmation"
+              placeholder="Password confirmation"
+              aria-label="Password confirmation"
+              onChange={handleChange}
+            />
+            <p className="text-center text-red-600">
+              {validationErrors.passwordConfirmation ||
+                serverValidationErrors.passwordConfirmation}
             </p>
             <button
               disabled={loading}
