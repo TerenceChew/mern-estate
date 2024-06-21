295270a92352f2f19043a446e55350f8d051659a
TerenceChew <wschew97@gmail.com>
2024-06-21 23:42:13 +0800

    Feature: Add error boundary for better user experience

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,15 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import { ErrorBoundary } from "react-error-boundary";
+import Fallback from "./components/Fallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
