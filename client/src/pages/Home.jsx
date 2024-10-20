a48e314ac9355e4b4a51e89626c452e4ee604224
TerenceChew <wschew97@gmail.com>
2024-10-21 00:26:17 +0800

    Refactor: Remove error from useEffect dependency array in Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -90,7 +90,7 @@ export default function Home() {
     fetchListingsWithOffer();
     fetchListingsForRent();
     fetchListingsForSale();
-  }, [error]);
+  }, []);
 
   // Swiper
   const swiperStyle = {
