fb0aaaea4d3fe6c4e40065cfbcb4238e6529ab78
TerenceChew <wschew97@gmail.com>
2024-11-08 01:06:00 +0800

    Refactor: Move validation func in SignUp to its own module

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,6 +7,7 @@ import {
   signUpFailure,
 } from "../redux/user/userSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { validateSignUp } from "../validations/auth.validation.js";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({
@@ -22,38 +23,6 @@ export default function SignUp() {
   const [submitRequested, setSubmitRequested] = useState(false);
   const [serverValidationErrors, setServerValidationErrors] = useState({});
 
-  // Validation
-  const validate = (formData) => {
-    const errors = {};
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/;
-
-    if (!formData.username) {
-      errors.username = "Please enter a valid username!";
-    } else if (formData.username.length < 5) {
-      errors.username = "Username must be at least 5 characters!";
-    } else if (formData.username.length > 20) {
-      errors.username = "Username cannot be more than 20 characters!";
-    }
-
-    if (!formData.email || !emailRegex.test(formData.email)) {
-      errors.email = "Please enter a valid email!";
-    }
-
-    if (!formData.password) {
-      errors.password = "Please enter a valid password!";
-    } else if (formData.password.length < 8) {
-      errors.password = "Password must be at least 8 characters!";
-    } else if (formData.password.length > 16) {
-      errors.password = "Password cannot be more than 16 characters!";
-    } else if (!passwordRegex.test(formData.password)) {
-      errors.password =
-        "Password must contain at least 1 digit, 1 uppercase and 1 lowercase letter!";
-    }
-
-    return errors;
-  };
-
   // Handler functions
   const handleChange = (e) => {
     const { id, value } = e.target;
@@ -67,7 +36,7 @@ export default function SignUp() {
     e.preventDefault();
 
     setServerValidationErrors({});
-    setValidationErrors(validate(formData));
+    setValidationErrors(validateSignUp(formData));
     setSubmitRequested(true);
   };
 
