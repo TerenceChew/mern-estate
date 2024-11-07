3a486f5418d8a98c7d70751d772d41e42976ba44
TerenceChew <wschew97@gmail.com>
2024-11-08 00:44:09 +0800

    Refactor: Move validation func in Profile to its own module

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,7 @@ import { deleteImageFileFromFirebase } from "../utils/firebase.storage";
 import { extractImageFileNameFromUrl } from "../utils/utilities.js";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { uploadImageFileToFirebase } from "../utils/firebase.storage.js";
+import { validate } from "../validations/user.validation.js";
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -42,45 +43,6 @@ export default function Profile() {
     password: "password",
     passwordConfirmation: "password",
   });
-
-  // Validation
-  const validate = (formData) => {
-    const { username, email, password, passwordConfirmation } = formData;
-    const errors = {};
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/;
-
-    if (!username) {
-      errors.username = "Please enter a valid username!";
-    } else if (username.length < 5) {
-      errors.username = "Username must be at least 5 characters!";
-    } else if (username.length > 20) {
-      errors.username = "Username cannot be more than 20 characters!";
-    }
-
-    if (!email || !emailRegex.test(email)) {
-      errors.email = "Please enter a valid email!";
-    }
-
-    if (!password) {
-      errors.password = "Please enter your current or new password!";
-    } else if (password.length < 8) {
-      errors.password = "Password must be at least 8 characters!";
-    } else if (password.length > 16) {
-      errors.password = "Password cannot be more than 16 characters!";
-    } else if (!passwordRegex.test(password)) {
-      errors.password =
-        "Password must contain at least 1 digit, 1 uppercase and 1 lowercase letter!";
-    }
-
-    if (!passwordConfirmation) {
-      errors.passwordConfirmation = "Please re-enter your password here!";
-    } else if (passwordConfirmation !== password) {
-      errors.passwordConfirmation = "Does not match password!";
-    }
-
-    return errors;
-  };
 
   // Handler functions
   const handleImgClick = () => {
