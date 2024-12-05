32d65249bfc13feb284f87ab9c66bdc28b0284a4
TerenceChew <wschew97@gmail.com>
2024-12-05 19:45:14 +0800

    Move create listing button from Profile to Listings for better user experience

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -4,6 +4,8 @@ import UserListingCard from "../components/UserListingCard";
 import DeleteConfirmationBox from "../components/DeleteConfirmationBox";
 import { extractImageFileNameFromUrl } from "../utils/utilities";
 import { deleteImageFileFromFirebase } from "../utils/firebase.storage";
+import { HiPlusCircle } from "react-icons/hi";
+import { TiPlus } from "react-icons/ti";
 
 export default function Listings() {
   const { id } = useParams();
@@ -90,7 +92,7 @@ export default function Listings() {
   return (
     <>
       <main
-        className={`min-h-dvh flex justify-center py-10 px-5 md:px-8 bg-gray-50 ${
+        className={`min-h-dvh flex justify-center relative pt-11 pb-10 px-5 md:px-8 bg-gray-50 ${
           deleteRequest.deleteRequested
             ? "pointer-events-none brightness-[.65] blur-sm"
             : ""
@@ -98,6 +100,19 @@ export default function Listings() {
       >
         <article className="w-full sm:max-w-2xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-screen-2xl flex flex-col items-center gap-8">
           <h1 className="font-semibold text-2xl sm:text-3xl">Your Listings</h1>
+
+          <Link
+            to="/create-listing"
+            className="flex justify-center items-center absolute right-4 xs:right-5 md:right-6 lg:right-8 xl:right-10 2xl:right-12 top-4 md:top-5 text-blue-700"
+            aria-label="Create new listing"
+            title="Create new listing"
+          >
+            <HiPlusCircle className="sm:hidden text-[28px] xs:text-[32px] hover:scale-[1.15] transition-transform duration-200" />
+            <span className="hidden sm:flex justify-center items-center gap-1 text-sm lg:text-[15px] xl:text-base font-poppins py-1.5 pl-1.5 pr-2.5 xl:pr-3 hover:bg-blue-700 hover:text-white transition-colors duration-500 rounded-md">
+              <TiPlus />
+              New Listing
+            </span>
+          </Link>
 
           {error ? (
             <p className="text-red-600 text-center" aria-label="Error message">
