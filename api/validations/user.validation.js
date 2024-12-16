10b8b3f442ccaf9dba6a9af6b730ea33db4a0f81
TerenceChew <wschew97@gmail.com>
2024-12-17 00:24:17 +0800

    Refactor: Remove unnecessary .isString() validations in user.validation

--- api/validations/user.validation.js
+++ api/validations/user.validation.js
@@ -4,9 +4,6 @@ import { validationResultHandler } from "./utilities.js";
 export const validateUpdateUser = [
   body("username")
     .trim()
-    .isString()
-    .withMessage("Username must be a string!")
-    .bail()
     .notEmpty()
     .withMessage("Username cannot be empty!")
     .bail()
@@ -17,9 +14,6 @@ export const validateUpdateUser = [
     .withMessage("Username cannot be more than 20 characters!"),
   body("email")
     .trim()
-    .isString()
-    .withMessage("Email must be a string!")
-    .bail()
     .notEmpty()
     .withMessage("Email cannot be empty!")
     .bail()
